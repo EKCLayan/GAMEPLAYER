de4718e62e860b4cb04db02bc9422ba1148ad1cd
EKCLayan <abilayananm2006@gmail.com>
2026-06-21 17:39:34 +0800

    Add wall movement and block jump functionality

--- GAMEPLAYER_WIP.ino
+++ GAMEPLAYER_WIP.ino
@@ -8,26 +8,36 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, 22, 21);
 #define GRID_SIZE 8
 
 int blockpp = 1;
+int wallpp = 128;
+int buttonUp = 27;
+int buttonstateUp = 1;
 int blockp = blockpp * GRID_SIZE;
+int blockhp = SCREEN_HEIGHT - GRID_SIZE;
+bool blocktopos = false;
 
 void setup() {
  u8g2.begin();
+ pinMode(buttonUp, INPUT_PULLUP);
+ //Serial.begin(9600);
 
 }
 
 void loop() {
  drawGame();
+ blockJump();
  moveBlock();
- 
+ moveWall();
 
 }
 
 void drawGame(){
   u8g2.clearBuffer();
-  u8g2.drawBox(blockp, SCREEN_HEIGHT - GRID_SIZE, GRID_SIZE, GRID_SIZE);
+  u8g2.drawBox(blockp, blockhp, GRID_SIZE, GRID_SIZE);
+  u8g2.drawBox(wallpp, 48, 4, 16);
   u8g2.sendBuffer();
   delay(100);
 
+  blockhp = SCREEN_HEIGHT - GRID_SIZE;
 
 }
 void moveBlock(){
@@ -36,5 +46,25 @@ void moveBlock(){
   blockp = blockpp * GRID_SIZE;
   delay(100);
  }
+ else{
+  blocktopos = true;
+ }
 }
 
+void moveWall(){
+ if(blocktopos == true && wallpp > 0){
+  wallpp = wallpp - 3;
+  delay(100);
+ }
+ else if(wallpp <= 0){
+  wallpp = 128;
+ }
+}
+void blockJump(){
+buttonstateUp = digitalRead(buttonUp);
+
+if(buttonstateUp == LOW){
+ //Serial.println("button pressed");
+ blockhp = blockhp - 25;
+}
+}
